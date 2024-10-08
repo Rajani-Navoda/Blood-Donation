8011d3444a4974ef0d90c588c8cfe22c889fa1b2
Rajani-Navoda <rajaninavoda@gmail.com>
2024-10-08 08:39:35 +0530

    add a nav bar UI

---
+++ front-end/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css',
+  imports: []
+})
+export class NavBarComponent {
+ 
+}
